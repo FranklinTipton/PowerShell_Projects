3222210188d1010e72d4b64c990067284d39779b
Franklin <fl.tipton1@gmail.com>
2020-09-21 12:22:18 -0700

    ProjectAuto formatted to create multiple scripts silmutaniously.

--- ProjectAuto/ProjectAuto.ps1
+++ ProjectAuto/ProjectAuto.ps1
@@ -1,9 +1,23 @@
-# Dot-Sourcing from BranchCreation to get Branch info
-. C:\Source\Repos\PowerShell_Projects\ProjectAuto\BranchCreationScript.ps1
+Write-Host "How many projects would you like to create?"
+Write-Host "You can create up to 5 projects at a time"
+$projectCount = Read-Host "Please enter a number between 1 and 5."
 
-# Dot Sourcing from ProjectFolderCreation to create files
-. C:\Source\Repos\PowerShell_Projects\ProjectAuto\ProjectFolderCreationScript.ps1
+while ($projectCount -notin (1..5))
+{ 
+    Write-Host "Your number was not between 1 and 5"
+    $projectCount = Read-Host "Please enter a number between 1 and 5."
+}
 
-git add *
-git commit -m "Project Creation"
-git push
+foreach ($projectCount in 1..$projectCount)
+{
+    # Dot-Sourcing from BranchCreation to get Branch info
+    . C:\Source\Repos\PowerShell_Projects\ProjectAuto\BranchCreationScript.ps1
+    # Dot Sourcing from ProjectFolderCreation to create files
+    . C:\Source\Repos\PowerShell_Projects\ProjectAuto\ProjectFolderCreationScript.ps1
+
+    git add *
+    git commit -m "Project Creation"
+    git push
+}
+
+Write-Host "That works!"
