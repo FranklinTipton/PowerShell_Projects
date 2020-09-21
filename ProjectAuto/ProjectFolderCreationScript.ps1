18d1b7aa7498ac0e016289d10096a70af2bb5607
Franklin <fl.tipton1@gmail.com>
2020-09-21 10:38:55 -0700

    Broke up scripts to modularize functionality

---
+++ ProjectAuto/ProjectFolderCreationScript.ps1
@@ -0,0 +1,19 @@
+# Variables
+$newProjectName = Read-Host -Prompt "Enter Project Name"
+$basepath = "C:\Source\Repos\PowerShell_Projects"
+
+# Add File 
+New-Item -Path "$basepath\$newProjectName" -ItemType Directory -force
+New-Item -Path "$basepath\$newProjectName\$newProjectName.ps1" -ItemType File
+
+# Create README
+$readMeContent = "#$newProjectName
+
+## Description
+
+## Ideas
+
+## Goals"
+
+New-Item -Path "$basepath\$newProjectName\README.md"  -ItemType File
+Set-Content -path "$basepath\$newProjectName\README.md" -Value  $readMeContent
